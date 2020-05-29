a36a18c84273df61f06b1450149b2764564b520c
JORAWARSINGHSAINI <jorawarsingh12@gmail.com>
2020-05-30 00:49:34 +0530

    6 commits

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/firestore'
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    authDomain: "eshopify-9a78b.firebaseapp.com",
+    databaseURL: "https://eshopify-9a78b.firebaseio.com",
+    projectId: "eshopify-9a78b",
+    storageBucket: "eshopify-9a78b.appspot.com",
+    messagingSenderId: "401660446117",
+    appId: "1:401660446117:web:1b52adaa5a50b1d87a944c",
+    measurementId: "G-74QTTR2TGW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+export  const storage=firebase.storage();
+export default  firebase

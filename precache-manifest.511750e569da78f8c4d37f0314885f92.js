52ca33b8611c6a088403129f6bf80a44f058dcdc
JORAWARSINGHSAINI <jorawarsingh12@gmail.com>
2020-05-30 00:54:10 +0530

    Updates

---
+++ precache-manifest.511750e569da78f8c4d37f0314885f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8bc373e14b59616841cc4b4709df6d",
+    "url": "/jorawarsinghsaini.github.io/eshopify/index.html"
+  },
+  {
+    "revision": "e358bad1f0b940ee5e99",
+    "url": "/jorawarsinghsaini.github.io/eshopify/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b2a55255449b4a9ba53",
+    "url": "/jorawarsinghsaini.github.io/eshopify/static/js/2.ea17e61f.chunk.js"
+  },
+  {
+    "revision": "7997f147006a576a088648683d0bcabd",
+    "url": "/jorawarsinghsaini.github.io/eshopify/static/js/2.ea17e61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e358bad1f0b940ee5e99",
+    "url": "/jorawarsinghsaini.github.io/eshopify/static/js/main.356c6786.chunk.js"
+  },
+  {
+    "revision": "dc7f01383dbe1715f2e3",
+    "url": "/jorawarsinghsaini.github.io/eshopify/static/js/runtime-main.48f05ff8.js"
+  }
+]);
